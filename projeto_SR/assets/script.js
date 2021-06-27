6150c34a52183668923c1b9efdd172cef26ea346
dmontei10 <72616022+dmontei10@users.noreply.github.com>
2021-06-27 23:07:16 +0100

    Add files via upload

---
+++ projeto_SR/assets/script.js
@@ -0,0 +1,14 @@
+  function clicarCobra(){
+    document.getElementById('carouselExampleIndicators').style.display = 'none';
+    document.getElementById('cobra600').style.display = 'block';
+    document.getElementById('irb140').style.display = 'none';
+  };
+
+  function clicarIrb(){
+    document.getElementById('carouselExampleIndicators').style.display = 'none';
+    document.getElementById('cobra600').style.display = 'none';
+    document.getElementById('irb140').style.display = 'block';
+  };
+
+  
+
